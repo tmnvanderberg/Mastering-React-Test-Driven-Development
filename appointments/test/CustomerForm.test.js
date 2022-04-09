72e56b6ae377a3224ba014552d834ffbc91e0270
Timon <timon@horus.nu>
2022-04-09 19:11:40 +0200

    wip

--- appointments/test/CustomerForm.test.js
+++ appointments/test/CustomerForm.test.js
@@ -4,14 +4,22 @@ import { CustomerForm } from '../src/CustomerForm';
 
 describe('CustomerForm', () => {
   let render, container;
+
+  const form = (id) => container.querySelector(`form[id=${id}]`);
+
   beforeEach(() => {
     ({ render, container } = createContainer());
   });
 
   it('renders a form', () => {
     render(<CustomerForm />);
-    expect(
-      container.querySelector('form[id="customer"]')
-    ).not.toBeNull();
+    expect(form('customer')).not.toBeNull();
+  });
+  it('renders the first name fiels as a text box', () => {
+    render(<CustomerForm />);
+    const field = form('customer').elements.firstName;
+    expect(field).not.toBeNull();
+    expect(field.tagName).toEqual('INPUT');
+    expect(field.type).toEqual('text');
   });
 });
